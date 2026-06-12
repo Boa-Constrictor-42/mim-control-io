b9d06e1ad39a35a5ee72eae3553e22454c7baa9a
GET <g.etlender@gmx.net>
2026-06-12 21:19:04 +0200

    feat: hamburger toggle in header, version under logo, status bar left

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Projects\MIM\web
 git add -A
-git commit -m "fix: sidebar overflow hidden + sb-top link styles"
+git commit -m "feat: hamburger toggle in header, version under logo, status bar left"
 git push origin main
 pause

4181331508a129a9aeb8b8b8a835c54008d0f34a
GET <g.etlender@gmx.net>
2026-06-12 20:55:16 +0200

    feat: collapsible sidebar + logo link

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Projects\MIM\web
 git add -A
-git commit -m "feat: sidebar navigation on all pages"
+git commit -m "feat: collapsible sidebar + logo link"
 git push origin main
 pause

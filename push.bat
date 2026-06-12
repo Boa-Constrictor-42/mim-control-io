bddc2fa3d96f3b7506d484bd384e52ef524a484b
GET <g.etlender@gmx.net>
2026-06-12 20:22:51 +0200

    feat: sidebar navigation on all pages

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Projects\MIM\web
 git add -A
-git commit -m "feat: profile button highlight - Default lights up; dirty state clears all"
+git commit -m "feat: sidebar navigation on all pages"
 git push origin main
 pause

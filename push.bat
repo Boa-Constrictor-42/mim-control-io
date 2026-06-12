e2df2670c8a7ca7996b5c96b7805bb7750d27e4e
GET <g.etlender@gmx.net>
2026-06-12 21:57:18 +0200

    feat: collapse at 1100px, status bar header right, logo as link, no sidebar logo

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Projects\MIM\web
 git add -A
-git commit -m "feat: centered header logo on all pages, status bar above mode buttons"
+git commit -m "feat: collapse at 1100px, status bar header right, logo as link, no sidebar logo"
 git push origin main
 pause

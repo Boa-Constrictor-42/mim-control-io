87955601234b64de78469f35163652d4ebd4c3e4
GET <g.etlender@gmx.net>
2026-06-12 20:58:57 +0200

    fix: sidebar overflow hidden + sb-top link styles

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d C:\Projects\MIM\web
 git add -A
-git commit -m "feat: collapsible sidebar + logo link"
+git commit -m "fix: sidebar overflow hidden + sb-top link styles"
 git push origin main
 pause
